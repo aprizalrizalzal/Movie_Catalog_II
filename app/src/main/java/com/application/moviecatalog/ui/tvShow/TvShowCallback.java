d68c65221497da2d16f3c6345368639471913120
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2022-01-14 18:56:00 +0800

    in_commit

--- app/src/main/java/com/application/moviecatalog/ui/tvShow/TvShowCallback.java
+++
@@ -1,8 +0,0 @@
-package com.application.moviecatalog.ui.tvShow;
-
-import com.application.moviecatalog.data.TvShowEntity;
-
-public interface TvShowCallback {
-    void onItemClick(TvShowEntity tvShowEntity);
-    void onShareClick(TvShowEntity tvShowEntity);
-}
